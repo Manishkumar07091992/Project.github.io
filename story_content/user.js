b1618cd0131e0bc93893cabc4981243faf249d4e
Manishkumar07091992 <156296553+Manishkumar07091992@users.noreply.github.com>
2024-04-22 15:18:24 +0530

    ManishKumarProject

---
+++ story_content/user.js
@@ -0,0 +1,601 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5mAlMsHTYA2":
+        Script1();
+        break;
+      case "5mb10XQEAhL":
+        Script2();
+        break;
+      case "6pEd6QHsCqd":
+        Script3();
+        break;
+      case "5z36qy7wxAV":
+        Script4();
+        break;
+      case "6NAMWIkZ3Ta":
+        Script5();
+        break;
+      case "6JtePvDcVeu":
+        Script6();
+        break;
+      case "6JyKpvWtyyl":
+        Script7();
+        break;
+      case "5Yp076EQbyn":
+        Script8();
+        break;
+      case "5oGU5nKJlAE":
+        Script9();
+        break;
+      case "6dxECzctTyY":
+        Script10();
+        break;
+      case "5eqOn1DfYPS":
+        Script11();
+        break;
+      case "5o8B33kZLms":
+        Script12();
+        break;
+      case "69lxxmkPu96":
+        Script13();
+        break;
+      case "5bQ5Oanal57":
+        Script14();
+        break;
+      case "6gcWmHCedwH":
+        Script15();
+        break;
+      case "6KoxyohUcia":
+        Script16();
+        break;
+      case "60WzRMSlj7s":
+        Script17();
+        break;
+      case "5Z2gj67fJxj":
+        Script18();
+        break;
+      case "6A94tnLjfmy":
+        Script19();
+        break;
+      case "5pEXHV6fWFx":
+        Script20();
+        break;
+      case "60p4bR8IQFQ":
+        Script21();
+        break;
+      case "6cj8j4h96Ee":
+        Script22();
+        break;
+      case "6KNZjM0PETq":
+        Script23();
+        break;
+      case "6DFlnHd0Ltz":
+        Script24();
+        break;
+      case "5ul7rQCtIB6":
+        Script25();
+        break;
+      case "63m0f3YRJRC":
+        Script26();
+        break;
+      case "6ClIy9AcsAg":
+        Script27();
+        break;
+      case "5fUI7qdxLOF":
+        Script28();
+        break;
+      case "6fyZ9ftTZZU":
+        Script29();
+        break;
+      case "5YGk6DOz4y8":
+        Script30();
+        break;
+      case "6Teh0Zoook5":
+        Script31();
+        break;
+      case "650YvEIbofO":
+        Script32();
+        break;
+      case "6b13bNmd4Dt":
+        Script33();
+        break;
+      case "6IR15qJnJx2":
+        Script34();
+        break;
+      case "5xZvQzuXVW1":
+        Script35();
+        break;
+      case "5WB1Mt5kGlK":
+        Script36();
+        break;
+      case "6X2FrwrnPQE":
+        Script37();
+        break;
+      case "6ABdXZlJpTe":
+        Script38();
+        break;
+      case "6icVEutOwmA":
+        Script39();
+        break;
+      case "6XT4Perooy7":
+        Script40();
+        break;
+      case "5en18QqNEJt":
+        Script41();
+        break;
+      case "5Vn4xuhm51A":
+        Script42();
+        break;
+      case "6jnvvHVzGX2":
+        Script43();
+        break;
+      case "5fMCia107Ek":
+        Script44();
+        break;
+      case "6koJJXeEtZY":
+        Script45();
+        break;
+      case "68ZVPZXS9Uq":
+        Script46();
+        break;
+      case "6Qb6kOKMIW5":
+        Script47();
+        break;
+      case "6P7ZImKtP6q":
+        Script48();
+        break;
+      case "5lY54N6YdmS":
+        Script49();
+        break;
+      case "5WD50usl17k":
+        Script50();
+        break;
+      case "6lQ8JNRRky3":
+        Script51();
+        break;
+      case "6HxaJ8jfCQm":
+        Script52();
+        break;
+      case "6jnlZsbA0fi":
+        Script53();
+        break;
+      case "67pbK11mRjl":
+        Script54();
+        break;
+      case "5sDtIguAcSx":
+        Script55();
+        break;
+      case "65l711Q5ecZ":
+        Script56();
+        break;
+      case "6j8bpQ6vHxD":
+        Script57();
+        break;
+      case "6ez76NDL61Y":
+        Script58();
+        break;
+      case "5mDaeVmX2wb":
+        Script59();
+        break;
+      case "690ujnkTndv":
+        Script60();
+        break;
+      case "5ZADL1EJBP6":
+        Script61();
+        break;
+      case "62TFrLFDbye":
+        Script62();
+        break;
+      case "5dbQCTrIyTt":
+        Script63();
+        break;
+      case "5zILIqwyxmj":
+        Script64();
+        break;
+      case "663yWslbrgX":
+        Script65();
+        break;
+      case "5avok2xgsE7":
+        Script66();
+        break;
+  }
+}
+
+function Script1()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script2()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script3()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script4()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script5()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script6()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
+function Script7()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script8()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script9()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script10()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script11()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script12()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
+function Script13()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script14()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script15()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script16()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script17()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script18()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
+function Script19()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script20()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script21()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script22()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script23()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script24()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
+function Script25()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script26()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script27()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script28()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script29()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script30()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
+function Script31()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script32()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script33()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script34()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script35()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script36()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
+function Script37()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script38()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script39()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script40()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script41()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script42()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
+function Script43()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script44()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script45()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script46()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script47()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script48()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
+function Script49()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script50()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script51()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script52()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script53()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script54()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
+function Script55()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script56()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script57()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script58()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script59()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script60()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
+function Script61()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0);
+}
+
+function Script62()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.2);
+}
+
+function Script63()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.4);
+}
+
+function Script64()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.6);
+}
+
+function Script65()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(0.8);
+}
+
+function Script66()
+{
+  var $volume = (GetPlayer().GetVar('volume') / 10);
+DS.appState.currentVolume(1);
+}
+
